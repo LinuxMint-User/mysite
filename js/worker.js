ca41944ff5cc2e65ae829cb3cdc84d4d2e5c4f4c
Eric Chan <55314107+LinuxMint-User@users.noreply.github.com>
2024-07-15 21:51:35 +0800

    Upload: worker,js

---
+++ js/worker.js
@@ -0,0 +1,44 @@
+// 创建一个函数来发送日志消息
+function sendLog(message) {
+    self.postMessage({ log: message });
+}
+
+// 重写 console.log 以使用 sendLog 函数
+console.log = function (message) {
+    sendLog(message);
+};
+
+self.addEventListener('message', function (e) {
+    if (e.data === 'terminate') {
+        self.postMessage({ finished: true, terminated: true }); // 发送终止确认消息
+        return; // 终止当前执行的代码
+    }
+
+    try {
+        // 执行用户代码
+        let result = eval(e.data);
+
+        // 检查结果是否为 Promise
+        if (result && typeof result.then === 'function') {
+            // 如果是 Promise，等待其解决
+            result.then(
+                res => {
+                    // 发送成功结果之前，发送所有日志
+                    self.postMessage({ success: res });
+                    self.postMessage({ finished: true }); // 表示任务完成
+                },
+                err => {
+                    self.postMessage({ error: err });
+                    self.postMessage({ finished: true }); // 表示任务完成
+                }
+            );
+        } else {
+            // 如果不是 Promise，直接发送结果
+            self.postMessage({ success: result });
+            self.postMessage({ finished: true }); // 表示任务完成
+        }
+    } catch (error) {
+        self.postMessage({ error: error.message });
+        self.postMessage({ finished: true }); // 表示任务完成
+    }
+});
